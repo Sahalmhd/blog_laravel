f44d3d455cca0341a18771d89eb39085f924dad1
sahalmhd <sahalmhd25@gmail.com>
2024-04-23 16:14:31 +0530

    new commit

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,24 @@
 <?php
 
-use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
 
-Route::get('', [AuthController::class,'index'])->name('index');
-Route::get('signup',[AuthController::class,'signup'])->name('signup');
-
-Route::post('/login',[AuthController::class,'auth'])->name('login');
-
+// Authentication Routes
+Route::get('/', [AuthController::class, 'index'])->name('index');
+Route::get('signup', [AuthController::class, 'signup'])->name('signup');
+Route::post('/login', [AuthController::class, 'auth'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('/dashboard',[AdminController::class,'dashboard'])->name('admin.dashboard')->middleware('auth');
 
+// Admin Routes
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+});
 
-Route::get('/udashboard',[UserController::class,'show'])->name('user.dashboard')->middleware('auth');;
-Route::get('/postblog',[UserController::class,'showblogpg'])->name('user.postblog')->middleware('auth');;
-Route::post('/post',[UserController::class,'post_blog'])->name('post');
-
-
+// User Routes
+Route::middleware('auth')->group(function () {
+    Route::get('/udashboard', [UserController::class, 'show'])->name('user.dashboard');
+    Route::get('/postblog', [UserController::class, 'showblogpg'])->name('user.postblog');
+    Route::post('/post', [UserController::class, 'post_blog'])->name('post');
+});
